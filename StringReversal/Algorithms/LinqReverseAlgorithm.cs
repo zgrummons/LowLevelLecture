8c80632861929886b39f063c75354122e66d7cfb
Zachariah Grummons <zgrummons@gmail.com>
2020-10-29 02:59:59 -0500

    added benchmarker and linq algorithm
    todo: more reversal algorithms

---
+++ StringReversal/Algorithms/LinqReverseAlgorithm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StringReversal.Algorithms
+{
+    public class LinqReverseAlgorithm : IAlgorithm
+    {
+        public string ReverseString(string input)
+        {
+            return new string(input.Reverse().ToArray());
+        }
+    }
+}
